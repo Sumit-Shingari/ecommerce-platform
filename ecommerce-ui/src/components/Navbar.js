d2cd86dfcca625cca3647e10bd707fed4894624d
Sumit Shingari <incsnjf4@amway.com>
2026-04-20 10:50:21 +0530

    nagp-final

--- ecommerce-ui/src/components/Navbar.js
+++ ecommerce-ui/src/components/Navbar.js
@@ -77,7 +77,7 @@ const Navbar = () => {
               className="fashion-logo"
               onClick={() => navigate("/")}
             >
-              StyleSphere
+              Style&Fashion
             </div>
 
             <nav className="fashion-menu">
